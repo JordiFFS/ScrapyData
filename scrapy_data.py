bc67135ce75b8b7147a8b149afcb2f29b939ff7a
Jordi Fiallos <jordifernandofiallos@gmail.com>
2024-11-15 10:36:57 -0500

    mas cambios

--- scrapy_data.py
+++ scrapy_data.py
@@ -2,43 +2,44 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 
-print('*** Sistema para descargar datos de tablas en html ***')
-url = input('Ingrese la url de la pagina que desea scrapiar: ')
-cantidad = int(input('Ingrese la cantidad de datos que desea descargar: '))
-numero_tabla = int(input('A que tabla del HTML quiere extraer la informacion: '))
+print('*** Sistema para descargar datos de tablas en HTML ***')
+url = input('Ingrese la URL de la página que desea scrapear. ')
+cantidad = int(input('Ingrese la cantidad de datos que desea descargar. '))
+numero_tabla = int(input('¿A qué tabla del HTML quiere extraer la información? '))
+
 print('<------------------------------------------------------->')
 
-# Funcion para convertir una lista en elementos del dataframe de pandas
+# Función para convertir una lista en elementos de un DataFrame de pandas
 def rowsToDataFrame(rows):
     df = pd.DataFrame(rows[1:], columns=rows[0])
     return df
 
 
-# Funcion para extraer los datos de una tabla HTML
+# Función para extraer los datos de una tabla HTML
 def processTableData(tbl):
     rows = []
-    for tr in tbl.find('tbody').find_all('tr'):  # itera los items internos de la tabla [<tr>]
+    for tr in tbl.find('tbody').find_all('tr'):  #Itera los elementos internos de la tabla (<tr>).
         row = []
-        for cell in tr.find_all(['td', 'th']): # consultando en cada celda si es [<td> o <th>]
-            item = cell.get_text(strip=True) # Trae el texto ignorando las demas etiquetas Ej: [<b>] los espacios
-            row.append(item) #añade a la lista row
+        for cell in tr.find_all(['td', 'th']): #Consulta en cada celda si es <td> o <th>.
+            item = cell.get_text(strip=True) #Extrae el texto ignorando las demás etiquetas (por ejemplo, <b>), y también los espacios.
+            row.append(item) #Añade el contenido extraído a la lista row.
         if row:
-            rows.append(row) # si row tiene información añade a la lista de rows
+            rows.append(row) #Si row tiene información, la añade a la lista de rows.
     return rows
 
 
-# funcion para extraer los datos URL y procesar el contenido HTML
+#Función para extraer los datos de una URL y procesar el contenido HTML
 def processDataHTML(data):
 
 
-    soup = bs(data, 'html.parser')  #Le da estructura al HTML que esta como string
-    tbl = soup.find_all('table')[numero_tabla-1]  # recupera la tabla del HTML
-    tblRows = processTableData(tbl)  # Ejecucion de la funcion para procesar el contenido de la tabla
+    soup = bs(data, 'html.parser')  #Estructura el HTML que está en formato de cadena (string).
+    tbl = soup.find_all('table')[numero_tabla-1]  #Recupera la tabla del HTML.
+    tblRows = processTableData(tbl)  # Ejecución del procesamiento de la función para el contenido de la tabla
     return tblRows
 
 
-# Leer la webside
-# Declaramos r para hacer lña consulta requests.get('URL')
+#Leer el sitio web.
+#Declarar req para hacer la consulta: requests.get('URL').
 req = requests.get(url)
 if req.status_code != 200:
     print(f'Error al acceder a la pagina: {req.status_code}')
@@ -47,11 +48,12 @@ if req.status_code != 200:
 print(f'Estado de la solicitud: {req.status_code}')
 
 
-HTML = req.text #si se trabaja con content te devuelve en un byte, por eso se coloca el .text para que
-                #se haga un string, y si se quiere un json se pone .json()
-table = processDataHTML(HTML) #No use json ya que es el texto HTML de lo que se esta obteniendo.
+HTML = req.text #Si se trabaja con content, devuelve un byte. Por eso, se utiliza .text para convertirlo a una cadena (string).
+                #Si se requiere un JSON, se utiliza .json().
+table = processDataHTML(HTML) #No se usa .json() aquí, ya que el contenido es texto HTML.
 df = rowsToDataFrame(table)
-print("DataFrame:\n", df.head(cantidad)) #df.head(n) me muestra n registros que estan al inicio
+print("DataFrame:\n", df.head(cantidad)) #df.head(n) muestra los primeros n registros del DataFrame.
 df.info()
-df.to_csv('statsWorldCup.csv', index=False)   #aqui se genera un csv y el index tiene la funcion de
-                                                        #ocultar o mostrar la cabecera True oculta y False muestra
+df.to_csv('statsWorldCup.csv', index=False)   #Se genera un archivo CSV donde el índice puede ocultarse o mostrarse:
+                                                        #True: Oculta el índice.
+                                                        #False: Muestra el índice.
