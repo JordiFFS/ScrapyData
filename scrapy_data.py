d8c40cd50aa8345cb5d8feee65c0fc63fd9c4554
Jordi Fiallos <jordifernandofiallos@gmail.com>
2024-11-18 18:09:47 -0500

    Ultimo cambio

--- scrapy_data.py
+++ scrapy_data.py
@@ -1,6 +1,6 @@
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup as bs
+import requests #La librería request es una librería de Python que permite hacer peticiones HTTP
+import pandas as pd #Pandas está diseñada específicamente para la manipulación y el análisis de datos en el lenguaje
+from bs4 import BeautifulSoup as bs #sirve para extraer datos de archivos html
 
 print('*** Sistema para descargar datos de tablas en HTML ***')
 url = input('Ingrese la URL de la página que desea scrapear: ')
@@ -8,10 +8,12 @@ numero_tabla = int(input('¿A qué tabla del HTML quiere extraer la información
 
 print('<------------------------------------------------------->')
 
+
 # Función para convertir una lista en elementos de un DataFrame de pandas
 def rowsToDataFrame(rows):
     df = pd.DataFrame(rows[1:], columns=rows[0])
     return df
+
 
 # Función para extraer los datos de una tabla HTML
 def processTableData(tbl):
@@ -25,6 +27,7 @@ def processTableData(tbl):
             rows.append(row)  # Si row tiene información, la añade a la lista de rows.
     return rows
 
+
 # Función para extraer los datos de una URL y procesar el contenido HTML
 def processDataHTML(data):
     soup = bs(data, 'html.parser')  # Estructura el HTML.
@@ -32,9 +35,25 @@ def processDataHTML(data):
     tblRows = processTableData(tbl)  # Procesa la tabla.
     return tblRows
 
+
 # Leer el sitio web
 req = requests.get(url)
 dominio = url.split("//")[1].split("/")[0].replace(".", "-")
+
+"""
+url.split("//") divide la url en dos partes si url = "https://ejemplo.com/path" el resultado sera
+["https:", "ejemplo.com/path"]
+[1] Toma el segundo elemento de la lista generada en este caso "ejemplo.com/path"
+.split("/") divide el string anterior por el carácter "/" con el ejemplo ["ejemplo.com", "path"]
+[0] selecciona el primer elemento de la lista "ejmplo.com"
+.replace reemplza todos los "." que ecuentre y los cambio por "-" en el ejemplo quedaria asi "ejemplo-com"
+
+url.split("//") → ["https:", "www.ejemplo.com/path"]
+[1] → "www.ejemplo.com/path"
+.split("/") → ["www.ejemplo.com", "path"]
+[0] → "www.ejemplo.com"
+.replace(".", "-") → "www-ejemplo-com"
+"""
 
 if req.status_code != 200:
     print(f'Error al acceder a la página: {req.status_code}')
